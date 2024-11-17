00131c1b5081c23167da9fc136d6402e7fa93e05
Michal Stasiak <michal.stasiak@protonmail.com>
2024-11-17 12:09:14 +0100

    table methods map() and forEach()

---
+++ script.js
@@ -0,0 +1,45 @@
+// Treść zadania: Przekształć tablicę imion tak, aby każde imię było pisane dużymi literami.
+// Użyj metody map().
+// Wykorzystaj funkcje strzałkowe
+// Wyświetl przekształconą tablicę w konsoli
+
+// const names = ["Anna", "Bartek", "Cezary"];
+// const newNames = names.map((name) => name.toUpperCase());
+// console.log(newNames);
+
+// Zsumuj wszystkie wartości liczbowe w tablicy.
+// Użyj metody forEach().
+// Wykorzystaj funkcje strzałkowe.
+// Wyświetl wynik w konsoli
+
+// const numbers = [1, 2, 3, 4, 5];
+// let sum = 0;
+
+// numbers.forEach((number) => {
+//   sum += number;
+// });
+
+// console.log(sum);
+
+// Stwórz tablicę, która zawiera długości imion podanych w tablicy wejściowej names.
+// Użyj metody map() do przekształcenia tablicy imion na tablicę ich długości.
+// Wykorzystaj funkcje strzałkowe.
+// Wyświetl nową tablicę w konsoli
+
+// const names = ["Anna", "Bartek", "Cezary"];
+// const namesLength = names.map((name) => name.length);
+// console.log(namesLength);
+
+// Dla podanej tablicy obiektów reprezentujących osoby, wykorzystaj metodę forEach() do wyświetlenia informacji o każdej osobie w formacie "Imię: [imie], Nazwisko: [nazwisko]".
+// Użyj metody forEach() do iteracji przez tablicę obiektów.
+// W każdej iteracji wyświetl informacje o osobie.
+// Wykorzystaj funkcje strzałkowe.
+
+// const people = [
+//   { firstName: "Anna", lastName: "Kowalska" },
+//   { firstName: "Bartek", lastName: "Nowak" },
+// ];
+
+// people.forEach((element) => {
+//   console.log(`Imię: ${element.firstName}, Nazwisko: ${element.lastName}`);
+// });
